46f09065b7d09ce76f7f983832fb62268db07347
Matthew <matthew.d.baker20@gmail.com>
2021-06-22 10:02:48 -0500

    working on site, trading the stock and some site layout

--- src/main/java/com/stocks/site/controllers/StockController.java
+++ src/main/java/com/stocks/site/controllers/StockController.java
@@ -7,7 +7,10 @@ import com.stocks.site.repository.StockRepo;
 import com.stocks.site.repository.UserRepo;
 import com.stocks.site.service.UserService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @Controller
@@ -27,7 +30,26 @@ public class StockController {
     }
 
     @GetMapping("/trade")
-    public String viewAllStocksPage() {
+    public String viewAllStocksPage(Model model) {
+        List<Stock> stockList = stockDao.findAll();
+        model.addAttribute("stockList", stockList);
         return "stocks/trade";
+    }
+
+    @GetMapping("/trade/{id}")
+    public String viewAllStocksPage(Model model, @PathVariable(name = "id") long id) {
+        Stock stock = stockDao.getOne(id);
+        model.addAttribute("stock", stock);
+        return "stocks/view";
+    }
+
+    @PostMapping("/trade-stock/{id}")
+    public String tradeStock(@PathVariable(name = "id") long id, @RequestParam(name = "group1", required = false) String checkbox, @RequestParam(name = "amount") int amount) {
+        System.out.println("Stock trade happened");
+        System.out.println(amount);
+        if(checkbox != null) {
+            // selling stock, check if you have that many
+        }
+        return "redirect:/trade/" + id;
     }
 }
